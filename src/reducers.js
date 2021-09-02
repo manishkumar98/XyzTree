4b8a02610ca737de390e312123037c11baf6d030
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-09-02 22:20:40 +0530

    ssss

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,16 @@
 import data from "./data";
 function reducers(state = data, action) {
   if (action.type === "INCREMENT") {
-    //console.log("xyz",state);
+    console.log("xyz", state);
+    console.log("xyz1", action);
     //action.id.counter=action.id.counter+1
-    return {...state,action.id.counter+1}
+    state.counter = state.counter + 1;
+    console.log("xyz2", state.counter);
+    console.log("abc", action.id);
+    console.log("vbg", action.counter);
+    console.log("ss", state);
+    //state.data.filter((elem)=>elem.id===action.id?action.counter+1:action.counter)
+    return { ...state };
   }
   return state;
 }
@@ -13,3 +20,4 @@ id: "0"
 name: "Counter"
 counter: 0
 children: Array(3)*/
+//state.id
